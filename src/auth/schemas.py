0441fda960558d1faa0bbaf4127c5927f3f23160
vitaly <dudkin231@mail.ru>
2023-12-07 12:17:57 +0300

    added changes

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -3,6 +3,7 @@ from typing import Optional
 from fastapi_users import schemas
 from pydantic import field_validator, EmailStr, Field
 from src.auth.utils import phone_number_regex, check_password_regex
+from passlib.hash import bcrypt
 
 
 class UserRead(schemas.BaseUser[int]):
@@ -47,29 +48,29 @@ class UserCreate(schemas.BaseUserCreate):
     def confirm_password(cls, confirm_password, info):
         if confirm_password != info.data['password']:
             raise ValueError("confirm_password doesn't match password")
-        return confirm_password
+        return bcrypt.hash(confirm_password)
 
 
-class UserUpdate(schemas.BaseUserCreate):
-    username: str = Field(max_length=100)
-    email: EmailStr
-    phone_number: str
-    password: str
-    confirm_password: str
-    role_id: int
-    is_active: Optional[bool] = True
-    is_superuser: Optional[bool] = False
-    is_verified: Optional[bool] = False
-
-    @field_validator('password')
-    def validate_password(cls, password):
-        if not check_password_regex(password):
-            raise ValueError("Password has to be longer than 7 symbols, Latin only, at least 1 "
-                             "uppercase character, at least 1 special character $%&!")
-        return password
-
-    @field_validator('confirm_password')
-    def confirm_password(cls, confirm_password, info):
-        if confirm_password != info.data['password']:
-            raise ValueError("confirm_password doesn't match password")
-        return confirm_password
+# class UserUpdate(schemas.BaseUserCreate):
+#     username: str = Field(max_length=100)
+#     email: EmailStr
+#     phone_number: str
+#     password: str
+#     confirm_password: str
+#     role_id: int
+#     is_active: Optional[bool] = True
+#     is_superuser: Optional[bool] = False
+#     is_verified: Optional[bool] = False
+# #
+#     @field_validator('password')
+#     def validate_password(cls, password):
+#         if not check_password_regex(password):
+#             raise ValueError("Password has to be longer than 7 symbols, Latin only, at least 1 "
+#                              "uppercase character, at least 1 special character $%&!")
+#         return password
+#
+#     @field_validator('confirm_password')
+#     def confirm_password(cls, confirm_password, info):
+#         if confirm_password != info.data['password']:
+#             raise ValueError("confirm_password doesn't match password")
+#         return confirm_password
